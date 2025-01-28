3401865e143e4d79dd0e001652b466dbd149eb75
rashikbuksh <rashikbuksh@gmail.com>
2025-01-28 19:39:38 +0600

    feat: add CRUD routes and handlers for portfolio entities with schema validation

---
+++ src/routes/portfolio/club/routes.ts
@@ -0,0 +1,124 @@
+import * as HSCode from 'stoker/http-status-codes';
+import { jsonContent, jsonContentRequired } from 'stoker/openapi/helpers';
+import { createErrorSchema } from 'stoker/openapi/schemas';
+
+import { notFoundSchema } from '@/lib/constants';
+import * as param from '@/lib/param';
+import { createRoute, z } from '@hono/zod-openapi';
+
+import { insertSchema, patchSchema, selectSchema } from './utils';
+
+const tags = ['portfolio.club'];
+
+export const list = createRoute({
+  path: '/portfolio/club',
+  method: 'get',
+  tags,
+  responses: {
+    [HSCode.OK]: jsonContent(
+      z.array(selectSchema),
+      'The list of club',
+    ),
+  },
+});
+
+export const create = createRoute({
+  path: '/portfolio/club',
+  method: 'post',
+  request: {
+    body: jsonContentRequired(
+      insertSchema,
+      'The club to create',
+    ),
+  },
+  tags,
+  responses: {
+    [HSCode.OK]: jsonContent(
+      selectSchema,
+      'The created club',
+    ),
+    [HSCode.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(insertSchema),
+      'The validation error(s)',
+    ),
+  },
+});
+
+export const getOne = createRoute({
+  path: '/portfolio/club/{uuid}',
+  method: 'get',
+  request: {
+    params: param.uuid,
+  },
+  tags,
+  responses: {
+    [HSCode.OK]: jsonContent(
+      selectSchema,
+      'The requested club',
+    ),
+    [HSCode.NOT_FOUND]: jsonContent(
+      notFoundSchema,
+      'club not found',
+    ),
+    [HSCode.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(param.uuid),
+      'Invalid id error',
+    ),
+  },
+});
+
+export const patch = createRoute({
+  path: '/portfolio/club/{uuid}',
+  method: 'patch',
+  request: {
+    params: param.uuid,
+    body: jsonContentRequired(
+      patchSchema,
+      'The club updates',
+    ),
+  },
+  tags,
+  responses: {
+    [HSCode.OK]: jsonContent(
+      selectSchema,
+      'The updated club',
+    ),
+    [HSCode.NOT_FOUND]: jsonContent(
+      notFoundSchema,
+      'club not found',
+    ),
+    [HSCode.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(patchSchema)
+        .or(createErrorSchema(param.uuid)),
+      'The validation error(s)',
+    ),
+  },
+});
+
+export const remove = createRoute({
+  path: '/portfolio/club/{uuid}',
+  method: 'delete',
+  request: {
+    params: param.uuid,
+  },
+  tags,
+  responses: {
+    [HSCode.NO_CONTENT]: {
+      description: 'club deleted',
+    },
+    [HSCode.NOT_FOUND]: jsonContent(
+      notFoundSchema,
+      'club not found',
+    ),
+    [HSCode.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(param.uuid),
+      'Invalid id error',
+    ),
+  },
+});
+
+export type ListRoute = typeof list;
+export type CreateRoute = typeof create;
+export type GetOneRoute = typeof getOne;
+export type PatchRoute = typeof patch;
+export type RemoveRoute = typeof remove;
---
+++ src/routes/portfolio/department/utils.ts
@@ -0,0 +1,40 @@
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+
+import { dateTimePattern } from '@/utils';
+
+import { department, department_category_enum } from '../schema';
+
+//* crud
+export const selectSchema = createSelectSchema(department);
+
+export const insertSchema = createInsertSchema(
+  department,
+  {
+    uuid: schema => schema.uuid.length(21),
+    name: schema => schema.name.min(1),
+    faculty_uuid: schema => schema.faculty_uuid.length(21),
+    created_by: schema => schema.created_by.length(21),
+    category: schema => schema.category.refine(value => department_category_enum(value), {
+      message: 'Invalid category',
+    }),
+    created_at: schema => schema.created_at.regex(dateTimePattern, {
+      message: 'created_at must be in the format "YYYY-MM-DD HH:MM:SS"',
+    }),
+    updated_at: schema => schema.updated_at.regex(dateTimePattern, {
+      message: 'updated_at must be in the format "YYYY-MM-DD HH:MM:SS"',
+    }),
+  },
+).required({
+  uuid: true,
+  name: true,
+  faculty_uuid: true,
+  category: true,
+  created_at: true,
+  created_by: true,
+}).omit({
+  id: true,
+  updated_at: true,
+  remarks: true,
+});
+
+export const patchSchema = insertSchema.partial();
