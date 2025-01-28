952fac28ae7a1ff8c3cae38631538dea4e4c473e
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-01-28 16:53:09 +0600

    feat: add office routes with CRUD operations and validation schemas

---
+++ src/routes/portfolio/office/utils.ts
@@ -0,0 +1,33 @@
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+
+import { dateTimePattern } from '@/utils';
+
+import { office } from '../schema';
+
+//* crud
+export const selectSchema = createSelectSchema(office);
+
+export const insertSchema = createInsertSchema(
+  office,
+  {
+    uuid: schema => schema.uuid.length(21),
+    created_by: schema => schema.created_by.length(21),
+    created_at: schema => schema.created_at.regex(dateTimePattern, {
+      message: 'created_at must be in the format "YYYY-MM-DD HH:MM:SS"',
+    }),
+    updated_at: schema => schema.updated_at.regex(dateTimePattern, {
+      message: 'updated_at must be in the format "YYYY-MM-DD HH:MM:SS"',
+    }),
+  },
+).required({
+  uuid: true,
+  category: true,
+  created_at: true,
+  created_by: true,
+}).omit({
+  id: true,
+  updated_at: true,
+  remarks: true,
+});
+
+export const patchSchema = insertSchema.partial();
