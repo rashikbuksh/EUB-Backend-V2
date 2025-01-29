b23d873376d9c6a1d4a3c6203ce09f4546307a28
Rashik Buksh <rashikbuksh@gmail.com>
2025-01-29 17:04:51 +0600

    feat: enhance authorities list handler to include user details from HR schema

--- src/routes/portfolio/authorities/handlers.ts
+++ src/routes/portfolio/authorities/handlers.ts
@@ -4,6 +4,7 @@ import { eq } from 'drizzle-orm';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
+import * as hrSchema from '@/routes/hr/schema';
 import { createToast, DataNotFound, ObjectNotFound } from '@/utils/return';
 
 import type { CreateRoute, GetOneRoute, ListRoute, PatchRoute, RemoveRoute } from './routes';
@@ -56,7 +57,21 @@ export const remove: AppRouteHandler<RemoveRoute> = async (c: any) => {
 };
 
 export const list: AppRouteHandler<ListRoute> = async (c: any) => {
-  const data = await db.query.authorities.findMany();
+  const resultPromise = db.select({
+    uuid: authorities.uuid,
+    user_uuid: authorities.user_uuid,
+    category: authorities.category,
+    short_biography: authorities.short_biography,
+    created_at: authorities.created_at,
+    updated_at: authorities.updated_at,
+    user_name: hrSchema.users.name,
+    user_designation: hrSchema.designation.name,
+    user_department: hrSchema.department.name,
+    user_email: hrSchema.users.email,
+    user_phone: hrSchema.users.phone,
+  }).from(authorities).leftJoin(hrSchema.users, eq(authorities.user_uuid, hrSchema.users.uuid)).leftJoin(hrSchema.designation, eq(hrSchema.users.designation_uuid, hrSchema.designation.uuid)).leftJoin(hrSchema.department, eq(hrSchema.users.department_uuid, hrSchema.department.uuid));
+
+  const data: any[] = await resultPromise;
 
   return c.json(data || [], HSCode.OK);
 };
