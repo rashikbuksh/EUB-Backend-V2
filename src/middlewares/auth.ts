6f7ba09e28103adf03ece5dfbefb8639c93ec9e3
Rashik Buksh <rashikbuksh@gmail.com>
2025-02-04 02:48:35 +0600

    fix: update token verification to allow POST requests for both signin and portfolio endpoints

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -23,7 +23,7 @@ export async function CreateToken(payload: JWTPayload) {
 export async function VerifyToken(token: string, c: Context) {
   const { url, method } = c.env.outgoing.req;
 
-  if (url === '/v1/signin' && method === 'POST') {
+  if (url === '/v1/signin' && url === '/v1/portfolio' && method === 'POST') {
     return true;
   }
 
