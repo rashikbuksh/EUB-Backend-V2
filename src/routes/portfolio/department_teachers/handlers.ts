3497c1696f9b024cc0ae4ffd5b325548d92a5611
Rashik Buksh <rashikbuksh@gmail.com>
2025-02-03 18:15:09 +0600

    feat: enhance department teachers list handler to include department and teacher names

--- src/routes/portfolio/department_teachers/handlers.ts
+++ src/routes/portfolio/department_teachers/handlers.ts
@@ -4,11 +4,12 @@ import { eq } from 'drizzle-orm';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
+import { users } from '@/routes/hr/schema';
 import { createToast, DataNotFound, ObjectNotFound } from '@/utils/return';
 
 import type { CreateRoute, GetOneRoute, ListRoute, PatchRoute, RemoveRoute } from './routes';
 
-import { department_teachers } from '../schema';
+import { department, department_teachers } from '../schema';
 
 export const create: AppRouteHandler<CreateRoute> = async (c: any) => {
   const value = c.req.valid('json');
@@ -56,7 +57,17 @@ export const remove: AppRouteHandler<RemoveRoute> = async (c: any) => {
 };
 
 export const list: AppRouteHandler<ListRoute> = async (c: any) => {
-  const data = await db.query.department_teachers.findMany();
+  const resultPromise = db.select({
+    name: department_teachers.id,
+    uuid: department_teachers.uuid,
+    department_uuid: department_teachers.department_uuid,
+    department_name: department.name,
+    teacher_uuid: department_teachers.teacher_uuid,
+    teacher_name: users.name,
+    department_head: department_teachers.department_head,
+  }).from(department_teachers);
+
+  const data = await resultPromise;
 
   return c.json(data || [], HSCode.OK);
 };
