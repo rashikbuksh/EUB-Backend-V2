51a68d5f79d3f553d28e5d495a410e3655e1da2e
Shahadat Anik <git.shahadat.anik@gmail.com>
2025-02-10 17:30:42 +0600

    feat: add contact-us route to public routes in auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -31,12 +31,13 @@ export function isPublicRoute(url: string, method: string) {
     { url: '/v1/signin', method: 'POST' },
     { url: '/v1/portfolio', method: 'GET' },
     { url: '/v1/portfolio/online-admission', method: 'POST' },
+    { url: '/v1/portfolio/contact-us', method: 'POST' },
   ];
 
   return publicUrls.some(route => url.startsWith(route.url) && route.method === method);
 }
 
-export const ALLOWED_ROUTES = [
+export const ALLOWED_ROUTES: string[] = [
   'http://localhost:3005',
   'http://localhost:3000',
   'http://103.147.163.46:4020',
