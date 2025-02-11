87fa480ad148b09552a0c5c6a06af724c597aaf3
Rashik Buksh <rashikbuksh@gmail.com>
2025-02-11 15:01:37 +0600

    fix: enhance portfolio query filtering with additional conditions

--- src/routes/portfolio/routine/handlers.ts
+++ src/routes/portfolio/routine/handlers.ts
@@ -1,6 +1,6 @@
 import type { AppRouteHandler } from '@/lib/types';
 
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
@@ -128,14 +128,40 @@ export const list: AppRouteHandler<ListRoute> = async (c: any) => {
     .leftJoin(department, eq(routine.department_uuid, department.uuid))
     .leftJoin(hrSchema.users, eq(routine.created_by, hrSchema.users.uuid));
 
-  if (portfolio_department)
+  if (portfolio_department && program && type) {
+    resultPromise.where(and(
+      eq(department.name, portfolio_department),
+      eq(routine.programs, program),
+      eq(routine.type, type),
+    ));
+  }
+  else if (portfolio_department && program) {
+    resultPromise.where(and(
+      eq(department.name, portfolio_department),
+      eq(routine.programs, program),
+    ));
+  }
+  else if (portfolio_department && type) {
+    resultPromise.where(and(
+      eq(department.name, portfolio_department),
+      eq(routine.type, type),
+    ));
+  }
+  else if (program && type) {
+    resultPromise.where(and(
+      eq(routine.programs, program),
+      eq(routine.type, type),
+    ));
+  }
+  else if (portfolio_department) {
     resultPromise.where(eq(department.name, portfolio_department));
-
-  if (program)
+  }
+  else if (program) {
     resultPromise.where(eq(routine.programs, program));
-
-  if (type)
+  }
+  else if (type) {
     resultPromise.where(eq(routine.type, type));
+  }
 
   const data = await resultPromise;
 
