3f65be8b221dc148b4cd726e6daefb9b77e540aa
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-03-05 09:43:22 +0600

    feat: add patch routes for various procure entities

--- src/routes/procure/vendor/index.ts
+++ src/routes/procure/vendor/index.ts
@@ -6,6 +6,7 @@ import * as routes from './routes';
 const router = createRouter()
   .openapi(routes.list, handlers.list)
   .openapi(routes.create, handlers.create)
+  .openapi(routes.patch, handlers.patch)
   .openapi(routes.getOne, handlers.getOne)
   .openapi(routes.remove, handlers.remove);
 
