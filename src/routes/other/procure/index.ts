9d78980a434decc8bf46ff037886226932e40bcc
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-04-13 18:35:11 +0600

    feat: add internal cost center routes and handlers

--- src/routes/other/procure/index.ts
+++ src/routes/other/procure/index.ts
@@ -1,6 +1,8 @@
 import category from './category';
+import internal_cost_center from './internal_cost_center';
+import item from './item';
 import purchase_cost_center from './purchase_cost_center';
 import sub_category from './sub_category';
 import vendor from './vendor';
 
-export default [category, sub_category, purchase_cost_center, vendor];
+export default [category, sub_category, purchase_cost_center, vendor, item, internal_cost_center];
