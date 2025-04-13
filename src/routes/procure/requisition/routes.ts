61371318d07055685ef8a1894074b93f64025657
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-04-13 18:57:47 +0600

    feat: add route and handler for retrieving item requisition details by UUID

--- src/routes/procure/requisition/routes.ts
+++ src/routes/procure/requisition/routes.ts
@@ -122,8 +122,32 @@ export const remove = createRoute({
   },
 });
 
+export const getItemRequisitionDetailsByRequisitionUuid = createRoute({
+  path: '/procure/item-requisition-details/by/{uuid}',
+  method: 'get',
+  request: {
+    params: param.uuid,
+  },
+  tags,
+  responses: {
+    [HSCode.OK]: jsonContent(
+      z.array(selectSchema),
+      'The requested requisition',
+    ),
+    [HSCode.NOT_FOUND]: jsonContent(
+      notFoundSchema,
+      'requisition not found',
+    ),
+    [HSCode.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(param.uuid),
+      'Invalid id error',
+    ),
+  },
+});
+
 export type ListRoute = typeof list;
 export type CreateRoute = typeof create;
 export type GetOneRoute = typeof getOne;
 export type PatchRoute = typeof patch;
 export type RemoveRoute = typeof remove;
+export type GetItemRequisitionDetailsByRequisitionUuidRoute = typeof getItemRequisitionDetailsByRequisitionUuid;
