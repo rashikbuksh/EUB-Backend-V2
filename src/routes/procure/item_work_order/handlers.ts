2309a9d3bbd7e3ab7364a60285b9880dc55b0e83
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-04-17 16:37:56 +0600

    feat: enhance getWorkOrderDetailsByWorkOrderUuid to include item details and aggregate item_work_order_entry

--- src/routes/procure/item_work_order/handlers.ts
+++ src/routes/procure/item_work_order/handlers.ts
@@ -5,13 +5,12 @@ import { eq, sql } from 'drizzle-orm';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
-import { PG_DECIMAL_TO_FLOAT } from '@/lib/variables';
 import * as hrSchema from '@/routes/hr/schema';
 import { createToast, DataNotFound, ObjectNotFound } from '@/utils/return';
 
 import type { CreateRoute, GetOneRoute, GetWorkOrderDEtailsByWorkOrderUuidRoute, ListRoute, PatchRoute, RemoveRoute } from './routes';
 
-import { item_work_order, item_work_order_entry, vendor } from '../schema';
+import { item, item_work_order, item_work_order_entry, vendor } from '../schema';
 
 // const created_user = alias(hrSchema.users, 'created_user');
 
@@ -113,21 +112,57 @@ export const getOne: AppRouteHandler<GetOneRoute> = async (c: any) => {
 
 export const getWorkOrderDEtailsByWorkOrderUuid: AppRouteHandler<GetWorkOrderDEtailsByWorkOrderUuidRoute> = async (c: any) => {
   const { uuid } = c.req.valid('param');
-  const data = await db.query.item_work_order.findFirst({
-    where(fields, operators) {
-      return operators.eq(fields.uuid, uuid);
-    },
-    with: {
-      item_work_order_entry: {
-        extras: {
-          quantity: PG_DECIMAL_TO_FLOAT(item_work_order_entry.quantity).as('quantity'),
-          unit_price: PG_DECIMAL_TO_FLOAT(item_work_order_entry.unit_price).as('unit_price'),
-          name: sql`(SELECT item.name FROM procure.item WHERE item.uuid = ${item_work_order_entry.item_uuid})`.as('name'),
-        },
-        orderBy: (item_work_order_entry, { asc }) => [asc(item_work_order_entry.created_at)],
-      },
-    },
-  });
+
+  // const data = await db.query.item_work_order.findFirst({
+  //   where(fields, operators) {
+  //     return operators.eq(fields.uuid, uuid);
+  //   },
+  //   with: {
+  //     item_work_order_entry: {
+  //       extras: {
+  //         quantity: PG_DECIMAL_TO_FLOAT(sql`${item_work_order_entry}.quantity`).as('quantity'),
+  //         unit_price: PG_DECIMAL_TO_FLOAT(sql`${item_work_order_entry}.unit_price`).as('unit_price'),
+  //         name: sql`(SELECT item.name FROM procure.item WHERE item.uuid = ${sql`${item_work_order_entry}.item_uuid`})`.as('name'),
+  //       },
+  //       orderBy: (item_work_order_entry, { asc }) => [asc(item_work_order_entry.created_at)],
+  //     },
+  //   },
+  // });
+
+  const resultPromise = db.select({
+    uuid: item_work_order.uuid,
+    vendor_uuid: item_work_order.vendor_uuid,
+    vendor_name: vendor.name,
+    status: item_work_order.status,
+    created_at: item_work_order.created_at,
+    updated_at: item_work_order.updated_at,
+    created_by: item_work_order.created_by,
+    created_by_name: hrSchema.users.name,
+    remarks: item_work_order.remarks,
+    item_work_order_entry: sql`COALESCE(json_agg(json_build_object(
+      'uuid', item_work_order_entry.uuid,
+      'item_work_uuid', item_work_order_entry.item_work_order_uuid,
+      'item_uuid', item_work_order_entry.item_uuid,
+      'quantity', item_work_order_entry.quantity::float8,
+      'unit_price', item_work_order_entry.unit_price::float8,
+      'is_received', item_work_order_entry.is_received,
+      'received_date', item_work_order_entry.received_date,
+      'crated_by', item_work_order_entry.created_by,
+      'created_at', item_work_order_entry.created_at,
+      'updated_at', item_work_order_entry.updated_at,
+      'remarks', item_work_order_entry.remarks,
+      'name', item.name
+    )), '[]')`,
+  })
+    .from(item_work_order)
+    .leftJoin(hrSchema.users, eq(item_work_order.created_by, hrSchema.users.uuid))
+    .leftJoin(vendor, eq(item_work_order.vendor_uuid, vendor.uuid))
+    .leftJoin(item_work_order_entry, eq(item_work_order.uuid, item_work_order_entry.item_work_order_uuid))
+    .leftJoin(item, eq(item_work_order_entry.item_uuid, item.uuid))
+    .where(eq(item_work_order.uuid, uuid))
+    .groupBy(item_work_order.uuid, vendor.name, item_work_order.created_by, hrSchema.users.name, item_work_order.status, item_work_order.created_at, item_work_order.updated_at, item_work_order.remarks);
+
+  const data = await resultPromise;
 
   if (!data)
     return DataNotFound(c);
