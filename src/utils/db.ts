7beb9144eeb59ed0e8e16ac89105affb6e180555
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-04-23 12:55:42 +0600

    fix: remove onDelete property from Operation interface and DEFAULT_OPERATION

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,8 +1,8 @@
 import type { UpdateDeleteAction } from 'drizzle-orm/pg-core';
 
-interface Operation { onDelete: UpdateDeleteAction; onUpdate: UpdateDeleteAction }
+interface Operation { onUpdate: UpdateDeleteAction }
 
-export const DEFAULT_OPERATION: Operation = { onDelete: 'set null', onUpdate: 'cascade' };
+export const DEFAULT_OPERATION: Operation = { onUpdate: 'cascade' };
 
 export const DEFAULT_SEQUENCE = {
   startWith: 1,
