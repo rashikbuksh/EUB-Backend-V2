3fdbecbf2c3ea61a6393f5d3fb6e8e4a8158eda8
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-05-18 14:57:03 +0600

    feat: add sub_purchase_cost_center routes and handlers

---
+++ src/routes/other/procure/sub_purchase_cost_center/routes.ts
@@ -0,0 +1,28 @@
+import * as HSCode from 'stoker/http-status-codes';
+import { jsonContent } from 'stoker/openapi/helpers';
+
+import { createRoute, z } from '@hono/zod-openapi';
+
+const tags = ['other'];
+
+export const valueLabel = createRoute({
+  path: '/other/procure/sub-purchase-cost-center/value/label',
+  method: 'get',
+  tags,
+  request: {
+    query: z.object({
+      access: z.string().optional(),
+    }),
+  },
+  responses: {
+    [HSCode.OK]: jsonContent(
+      z.object({
+        value: z.string(),
+        label: z.string(),
+      }),
+      'The valueLabel of sub purchase cost center',
+    ),
+  },
+});
+
+export type ValueLabelRoute = typeof valueLabel;
