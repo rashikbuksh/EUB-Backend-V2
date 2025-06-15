fceefa00550e48334def2f0f60efa960b01ae3cd
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-06-15 18:47:54 +0600

    feat: add getQnsCategoryAndQnsDetails route and handler

--- src/routes/fde/qns_category/handlers.ts
+++ src/routes/fde/qns_category/handlers.ts
@@ -1,6 +1,6 @@
 import type { AppRouteHandler } from '@/lib/types';
 
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
@@ -8,7 +8,7 @@ import { PG_DECIMAL_TO_FLOAT } from '@/lib/variables';
 import { users } from '@/routes/hr/schema';
 import { createToast, DataNotFound, ObjectNotFound } from '@/utils/return';
 
-import type { CreateRoute, GetOneRoute, ListRoute, PatchRoute, RemoveRoute } from './routes';
+import type { CreateRoute, GetOneRoute, GetQnsCategoryAndQnsDetailsRoute, ListRoute, PatchRoute, RemoveRoute } from './routes';
 
 import { qns_category } from '../schema';
 
@@ -98,6 +98,54 @@ export const getOne: AppRouteHandler<GetOneRoute> = async (c: any) => {
 
   if (!data)
     return DataNotFound(c);
+
+  return c.json(data, HSCode.OK);
+};
+
+export const getQnsCategoryAndQnsDetails: AppRouteHandler<GetQnsCategoryAndQnsDetailsRoute> = async (c: any) => {
+  const resultPromise = db.select({
+    uuid: qns_category.uuid,
+    name: qns_category.name,
+    index: qns_category.index,
+    min_percentage: PG_DECIMAL_TO_FLOAT(qns_category.min_percentage),
+    created_by: qns_category.created_by,
+    created_by_name: users.name,
+    created_at: qns_category.created_at,
+    updated_at: qns_category.updated_at,
+    remarks: qns_category.remarks,
+    qns: sql`COALESCE(ARRAY(
+          SELECT jsonb_build_object(
+            'uuid', qns.uuid,
+            'name', qns.name,
+            'index', qns.index,
+            'active', qns.active,
+            'created_by', qns.created_by,
+            'created_by_name', users.name,
+            'created_at', qns.created_at,
+            'updated_at', qns.updated_at,
+            'remarks', qns.remarks
+          )
+          FROM fde.qns
+          WHERE qns.qns_category_uuid = ${qns_category.uuid} AND qns.active = true
+        ), ARRAY[]::jsonb[])`.as('qns'),
+  })
+    .from(qns_category)
+    .leftJoin(users, eq(users.uuid, qns_category.created_by))
+    .orderBy(qns_category.index);
+
+  const data = await resultPromise;
+
+  if (!data || data.length === 0)
+    return DataNotFound(c);
+
+  // const transformed = data.map((row) => {
+  //   const { qns, name, ...rest } = row;
+  //   return {
+  //     ...rest,
+  //     name,
+  //     [String(name)]: qns,
+  //   };
+  // });
 
   return c.json(data, HSCode.OK);
 };
