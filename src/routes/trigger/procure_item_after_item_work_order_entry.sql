2299d60498bff36ec7d2470a9d53c827448e8d82
rashikbuksh <rashikbuksh@gmail.com>
2025-06-23 15:18:08 +0600

    fix: refactor triggers to improve readability and ensure consistent handling of delivery statements

--- src/routes/trigger/procure_item_after_item_work_order_entry.sql
+++ src/routes/trigger/procure_item_after_item_work_order_entry.sql
@@ -1,39 +1,62 @@
 -- ? ----------------------- TRIGGER item_after_item_work_order_entry -------------------------
 CREATE OR REPLACE FUNCTION procure.item_after_item_work_order_entry_insert_function() RETURNS TRIGGER AS $$
+DECLARE 
+    delivery_statement_bool BOOLEAN;
 BEGIN
+    SELECT is_delivery_statement INTO delivery_statement_bool
+    FROM procure.item_work_order
+    WHERE uuid = NEW.item_work_order_uuid;
+
+    IF delivery_statement_bool IS TRUE THEN
         UPDATE procure.item
         SET
             quantity = quantity + NEW.provided_quantity
         WHERE uuid = NEW.item_uuid;
+    END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION procure.item_after_item_work_order_entry_delete_function() RETURNS TRIGGER AS $$
+DECLARE 
+    delivery_statement_bool BOOLEAN;
 BEGIN
+    SELECT is_delivery_statement INTO delivery_statement_bool
+    FROM procure.item_work_order
+    WHERE uuid = OLD.item_work_order_uuid;
+    
+    IF delivery_statement_bool IS TRUE THEN
         UPDATE procure.item
         SET
             quantity = quantity - OLD.provided_quantity
         WHERE uuid = OLD.item_uuid;
+    END IF;
     
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION procure.item_after_item_work_order_entry_update_function() RETURNS TRIGGER AS $$
+DECLARE 
+    delivery_statement_bool BOOLEAN;
 BEGIN
-    
+    SELECT is_delivery_statement INTO delivery_statement_bool
+    FROM procure.item_work_order
+    WHERE uuid = OLD.item_work_order_uuid;
+
+    IF delivery_statement_bool IS TRUE THEN
         UPDATE procure.item
         SET
             quantity = quantity - OLD.provided_quantity
         WHERE uuid = OLD.item_uuid;
 
-    
         UPDATE procure.item
         SET
             quantity = quantity + NEW.provided_quantity
-        WHERE uuid = NEW.item_uuid;
+        WHERE
+            uuid = NEW.item_uuid;
+    END IF;
 
     RETURN NEW;
 END;
