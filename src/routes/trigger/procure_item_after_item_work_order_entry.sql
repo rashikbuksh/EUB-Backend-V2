78b6928a8f567bc5237d6f99776182af546a774d
rashikbuksh <rashikbuksh@gmail.com>
2025-06-23 15:08:05 +0600

    fix: update triggers to use provided_quantity instead of quantity and remove unnecessary conditions

--- src/routes/trigger/procure_item_after_item_work_order_entry.sql
+++ src/routes/trigger/procure_item_after_item_work_order_entry.sql
@@ -1,46 +1,39 @@
 -- ? ----------------------- TRIGGER item_after_item_work_order_entry -------------------------
 CREATE OR REPLACE FUNCTION procure.item_after_item_work_order_entry_insert_function() RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.is_received = TRUE THEN 
         UPDATE procure.item
         SET
-            quantity = quantity + NEW.quantity
+            quantity = quantity + NEW.provided_quantity
         WHERE uuid = NEW.item_uuid;
-    END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-
 CREATE OR REPLACE FUNCTION procure.item_after_item_work_order_entry_delete_function() RETURNS TRIGGER AS $$
 BEGIN
-    IF OLD.is_received = TRUE THEN 
         UPDATE procure.item
         SET
-            quantity = quantity - OLD.quantity
+            quantity = quantity - OLD.provided_quantity
         WHERE uuid = OLD.item_uuid;
-    END IF;
-
+    
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION procure.item_after_item_work_order_entry_update_function() RETURNS TRIGGER AS $$
 BEGIN
-    IF OLD.is_received = TRUE THEN 
+    
         UPDATE procure.item
         SET
-            quantity = quantity - OLD.quantity
+            quantity = quantity - OLD.provided_quantity
         WHERE uuid = OLD.item_uuid;
-    END IF;
 
-    IF NEW.is_received = TRUE THEN 
+    
         UPDATE procure.item
         SET
-            quantity = quantity + NEW.quantity
+            quantity = quantity + NEW.provided_quantity
         WHERE uuid = NEW.item_uuid;
-    END IF;
 
     RETURN NEW;
 END;
