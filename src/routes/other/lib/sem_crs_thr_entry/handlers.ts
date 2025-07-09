9de8ad1e67e6b58e408c6bc01863287d0fa2d833
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-07-09 14:29:46 +0600

    feat: update valueLabel handler to include course name in label concatenation

--- src/routes/other/lib/sem_crs_thr_entry/handlers.ts
+++ src/routes/other/lib/sem_crs_thr_entry/handlers.ts
@@ -5,7 +5,7 @@ import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
 import { users } from '@/routes/hr/schema';
-import { course_section, sem_crs_thr_entry } from '@/routes/lib/schema';
+import { course, course_section, sem_crs_thr_entry } from '@/routes/lib/schema';
 import { teachers } from '@/routes/portfolio/schema';
 
 import type { ValueLabelRoute } from './routes';
@@ -13,10 +13,11 @@ import type { ValueLabelRoute } from './routes';
 export const valueLabel: AppRouteHandler<ValueLabelRoute> = async (c: any) => {
   const resultPromise = db.select({
     value: sem_crs_thr_entry.uuid,
-    label: sql`CONCAT( ${course_section.name}, ' - ', ${users.name} , ' - ', ${sem_crs_thr_entry.class_size})`.as('label'),
+    label: sql`CONCAT( ${course.name}, ' - ',${course_section.name}, ' - ', ${users.name} , ' - ', ${sem_crs_thr_entry.class_size})`.as('label'),
   })
     .from(sem_crs_thr_entry)
     .leftJoin(course_section, eq(course_section.uuid, sem_crs_thr_entry.course_section_uuid))
+    .leftJoin(course, eq(course.uuid, course_section.course_uuid))
     .leftJoin(teachers, eq(teachers.uuid, sem_crs_thr_entry.teachers_uuid))
     .leftJoin(users, eq(users.uuid, teachers.teacher_uuid));
 
