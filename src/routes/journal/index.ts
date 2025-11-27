a9d6af5a4f3b0984413b22490b05ef271c757d01
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-11-27 17:34:49 +0600

    feat: Add authors route to journal routes export

--- src/routes/journal/index.ts
+++ src/routes/journal/index.ts
@@ -2,9 +2,10 @@ import article_authors from './article_authors';
 import article_images from './article_images';
 import article_keywords from './article_keywords';
 import articles from './articles';
+import authors from './authors';
 import boards from './boards';
 import keywords from './keywords';
 import scope from './scope';
 import volume from './volume';
 
-export default [boards, volume, scope, keywords, articles, article_images, article_authors, article_keywords] as const;
+export default [boards, volume, scope, keywords, articles, article_images, article_authors, article_keywords, authors] as const;
